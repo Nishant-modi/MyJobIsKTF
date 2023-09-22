03bfb92f091a29b1d5ce5da9657c47ed55385d0f
Nishant-modi <mnishant0123@gmail.com>
2023-09-22 10:47:21 +0530

    1st commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Vector2 mouseTurn;
+    public float sensitivity = 1.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Confined;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mouseTurn.x += Input.GetAxis("Mouse X") * sensitivity;
+        mouseTurn.y += Input.GetAxis("Mouse Y") * sensitivity;
+        transform.rotation = Quaternion.Euler(-mouseTurn.y, mouseTurn.x, 0); 
+    }
+}
---
+++ Assets/Scripts/CursorController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorController : MonoBehaviour
+{
+
+    public Vector3 screenPos;
+    public Vector3 worldPos;
+    public LayerMask screen;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        screenPos = Input.mousePosition;
+
+        Ray ray = Camera.main.ScreenPointToRay(screenPos);
+
+        if(Physics.Raycast(ray, out RaycastHit hitInfo, 100, screen))
+        {
+            worldPos = hitInfo.point;
+        }
+        else
+        {
+            worldPos = Vector3.zero;
+        }
+        worldPos.z -= 0.05f;
+
+        transform.position = worldPos;
+    }
+}
